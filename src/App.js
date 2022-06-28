d5430d0df1e660ac025787d145275d572bbb2519
Bilal Shabbir <bilalshabbirkind@gmail.com>
2022-06-29 00:10:31 +0500

    Component Add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
+import React,{ useState } from 'react';
+import Student from './Student';
 import './App.css';
 
-function App() {
+function App(){
+const [name,setName]=useState("Bilal")
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World!!</h1>
+      <Student name={name} />
+      <button onClick={()=>{setName("Billo")}}>Update Button</button>
     </div>
   );
 }
-
 export default App;
