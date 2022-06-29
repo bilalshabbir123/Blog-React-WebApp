964ff1c8a8b00435ffedea35c355e918da1d094f
Bilal Shabbir <bilalshabbirkind@gmail.com>
2022-06-29 23:16:43 +0500

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import './App.css';
-import Login from './Login';
-
-function App(){
- 
- return(
-<div className='App'>
-  <h1>Hello Dominika </h1>
-  <Login/>
-</div>
- )
+import React from 'react';
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0
+    }
+  }
+  shouldComponentUpdate()
+  {
+    console.warn("shouldComponentUpdate",this.state.count);
+    return true;
+  }
+  render() {
+    return (
+       <div className='App'>
+        <h2 style={{ color: "red" }}>Update Count {this.state.count}</h2>
+        <button onClick={() => { this.setState({count:this.state.count + 1})}}>Update Count</button>
+      </div>
+    );
+  }
 }
-export default App;
